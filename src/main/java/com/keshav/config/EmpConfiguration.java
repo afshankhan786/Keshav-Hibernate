934d1fe78188209f367bb0a8b4e977a6f768e1e0
afshankhan786 <profit.afshankhan@gmail.com>
2026-01-20 22:05:16 +0530

    upsdate

---
+++ src/main/java/com/keshav/config/EmpConfiguration.java
@@ -0,0 +1,32 @@
+package com.keshav.config;
+
+import java.util.Properties;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.cfg.Environment;
+
+public class EmpConfiguration {
+
+	public static SessionFactory getSessionFactory() {
+
+		Properties properties = new Properties();
+
+		properties.put(AvailableSettings.DIALECT, "org.hibernate.dialect.MySQLDialect");
+		properties.put(AvailableSettings.JAKARTA_JDBC_DRIVER, "com.mysql.cj.jdbc.Driver");
+		properties.put(AvailableSettings.JAKARTA_JDBC_URL, "jdbc:mysql://localhost:3306/hibernate");
+		properties.put(AvailableSettings.JAKARTA_JDBC_USER, "root");
+		properties.put(AvailableSettings.JAKARTA_JDBC_PASSWORD, "root");
+		properties.put(AvailableSettings.HBM2DDL_AUTO, "create");
+		properties.put(AvailableSettings.SHOW_SQL, "true");
+		properties.put(Environment.FORMAT_SQL, "true");
+
+		return new MetadataSources(new StandardServiceRegistryBuilder().applySettings(properties).build())
+				.addAnnotatedClasses(com.keshav.entity.Employee.class,com.keshav.entity.Address.class)
+				.getMetadataBuilder().build().buildSessionFactory();
+
+	}
+
+}
